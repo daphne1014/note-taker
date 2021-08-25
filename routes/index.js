ed110e7d97a21b9fd4081a6a617ae4bd0ad942fa
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-08-24 23:14:17 -0700

    Add Heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const [notes] = require('../db/db.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
