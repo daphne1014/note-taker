c695b0b61592b8873b3a6f11bf41e85c06462a21
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-08-24 23:55:42 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const [ notes ] = require('./data/notes')
 
+const PORT = process.env.PORT || 3001;
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     console.log(req.query)
     res.json(results);
   });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
   });
