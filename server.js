5bde91526c97795639a4a75f637aee235a24580a
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-08-25 00:44:41 -0700

    Create Routes to Serve index.html

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 const { notes } = require('./data/notes')
 
@@ -73,7 +74,19 @@ app.post('/api/notes', (req, res) => {
         res.json(note);
     }
 });
-
+//HTMLRoutes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 app.listen(PORT, () => {
